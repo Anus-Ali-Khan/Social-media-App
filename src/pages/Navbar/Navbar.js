e55bd46c9d3ffa4f66dbdb6dfbb38850cd0015d2
Anus Ali Khan <anusalikhan1405@gmail.com>
2023-07-21 23:04:55 +0500

    navbar completed

--- src/pages/Navbar/Navbar.js
+++ src/pages/Navbar/Navbar.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../Navbar/Navbar.css';
-import { AiOutlineSearch } from "react-icons/ai"
+import { BsSearch } from "react-icons/bs";
+import { FaFacebook } from "react-icons/fa";
+import { BsTwitter } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
+import { FaBars } from "react-icons/fa";
 
 
 function Navbar() {
     return <div className='navbar'>
-        <nav>
+        <nav className='header'>
             <input type="checkbox" id="chk1" />
             <div className='logo'><h1>Social Influencer</h1></div>
             <div className='search-box'>
-                <form action="">
+                <form >
                     <input type="text" name='search' id='srch' placeholder='Search' />
-                    <button type='submit'><AiOutlineSearch /></button>
+                    <button type='submit'><BsSearch /></button>
                 </form>
             </div>
+            <ul>
+                <li><Link to='/' >Home </Link></li>
+                <li><Link to='/login' >Login </Link></li>
+                <li> <Link to='/signup' >Signup</Link></li>
+                <li> <Link to='/createpost' >createpost </Link></li>
+                <li className='icons'>
+                    <a href="#"><FaFacebook /></a>
+                    <a href="#"><BsTwitter /></a>
+                    <a href="#"><BsInstagram /></a>
+                </li>
+            </ul>
+            <div className='menu'>
+                <label htmlFor="chk1">
+                    <FaBars />
+                </label>
+            </div>
+
         </nav>
     </div>
 }
