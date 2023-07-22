f03af066ee2c5c04ceec7995ba75264c52283fdf
Anus Ali Khan <anusalikhan1405@gmail.com>
2023-07-23 00:00:12 +0500

    Sign up page working

--- src/pages/SignUp/Signup.js
+++ src/pages/SignUp/Signup.js
@@ -1,7 +1,44 @@
 import React from "react";
+import { BiSolidUser } from 'react-icons/bi'
+import { BiSolidLockAlt } from 'react-icons/bi'
+import { MdFacebook } from 'react-icons/md'
+import { AiFillTwitterCircle } from 'react-icons/ai'
+import { AiFillGoogleCircle } from 'react-icons/ai'
 
 function Signup() {
-    return <div>Signup</div>;
+
+    return <div className="login-container">
+        <div className="wrapper">
+            <form action="">
+                <h1>Sign Up</h1>
+                <div className="input-box">
+                    <input type="text" placeholder="Username" required />
+                    < BiSolidUser className="i" />
+                </div>
+                <div className="input-box">
+                    <input type="password" placeholder="Password" required />
+                    < BiSolidLockAlt className="i" />
+                </div>
+                <div className="remember-forgot">
+                    <label><input type="checkbox" />Remember me</label>
+                    <a href="#"> Forgot Password?</a>
+                </div>
+
+                <button type="submit" className="btn" >Submit</button>
+                <p className="signup">Or Sign Up Using</p>
+                <div className="icons">
+                    <a href="#">< MdFacebook id="f-icon" /></a>
+                    <a href="#">< AiFillTwitterCircle id="t-icon" /></a>
+                    <a href="#">< AiFillGoogleCircle id="g-icon" /></a>
+                </div>
+
+                <div className="register-link">
+                    <p>Don't have an account? <a href="#">Signup</a></p>
+                </div>
+            </form>
+        </div>
+
+    </div>;
 }
 
 export default Signup;
