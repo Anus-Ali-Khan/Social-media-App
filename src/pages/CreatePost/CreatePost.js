bfac71d5af63f698a1afa28890753434b55c1d90
Anus Ali Khan <anusalikhan1405@gmail.com>
2023-07-26 23:56:46 +0500

    working on createpost page

--- src/pages/CreatePost/CreatePost.js
+++ src/pages/CreatePost/CreatePost.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import '../CreatePost/CreatePost.css'
+import { Link } from 'react-router-dom';
 
 
 
-function CreatePost({ isAuth }) {
+function CreatePost() {
 
-    return <div>CreatePost
+    return <div className='background'>
+        <div className='nav'>
+            <h2><Link to='/'>Home</Link></h2>
+            <h2>Create Post</h2>
+            <button>Logout</button>
+        </div>
     </div>
 }
 
