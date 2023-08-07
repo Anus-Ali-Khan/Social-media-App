176c31189925d87d64b4b219f470bdb1147399aa
Anus Ali Khan <anusalikhan1405@gmail.com>
2023-08-07 19:17:52 +0500

    Working on display post

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,16 +1,34 @@
 import React from "react";
-import { useSelector } from "react-redux"
-import Navbar from '../Navbar/Navbar';
+import { useSelector } from "react-redux";
+import Navbar from "../Navbar/Navbar";
+import { collection, getDocs, doc } from "firebase/firestore";
+import { db, auth } from "../../firebase";
+import { useState, useEffect } from "react";
 
 function Home() {
+  const user = useSelector((state) => state.user.value);
+  const [postLists, setPostList] = useState([]);
+  const postsCollectionRef = collection(db, "posts");
 
-    const user = useSelector((state) => state.user.value)
-    console.log(user)
-    return <div>
-        <Navbar />
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postsCollectionRef);
+      setPostList(
+        data.docs.map((doc) => {
+          console.log(doc.data());
+          return { ...doc.data() };
+        })
+      );
+    };
+    getPosts();
+  }, []);
 
-
-    </div>;
+  return (
+    <div className="home">
+      <Navbar />
+      <div className="homePage"></div>
+    </div>
+  );
 }
 
 export default Home;
